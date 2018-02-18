ed9c4935797a7df3ea490f15f12c2f1277737fe0
David Parsons <dparson9@uncc.edu>
2018-02-18 03:06:08 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -54,12 +54,12 @@ class HangpersonApp < Sinatra::Base
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
+      @game.word
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
+    @game.word
     erb :lose # You may change/remove this line
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,17 +4,94 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  
+  attr_accessor :word, :game, :win, :lose, :play, :guesses, :show, :wrong_guesses
+  
+   def guess(letter)
+       
+       
+    if letter == nil or letter == "" or !letter.match(/[A-Za-z]/)
+        
+           throw "Error"
+           
+    end
+    
+    
 
-  # def initialize()
-  # end
+    if @guesses.downcase.include? letter.downcase or @wrong_guesses.downcase.include? letter.downcase
+           return false
+           
+    end
+           
+    if @word.downcase.include? letter.downcase
+            
+            @guesses += letter
+            
+    else
+            @wrong_guesses += letter
+            
+    end
+    
+   end
+        
+   
+    
+    
+    
+  
+  
+  def check_win_or_lose
+      
+   if @wrong_guesses.length == 7
+       
+      return :lose
+      
+   end
+  
+  
+  
+   if not word_with_guesses.include? "-"
+      
+      return :win
+      
+   end
+   
+  return :play
+  
+  end
+  
+  def word_with_guesses(word=@word,guesses=@guesses)
+      
+  word.gsub(/[^#{guesses}]/,'-')
+  
+  
+    
+  end
+  
+  
+  
+  
+  
+
+   
   
   def initialize(word)
+    
     @word = word
+    @game = game
+    @win = win
+    @lose = lose
+    @show = show
+    @guesses = ""
+    @wrong_guesses = ""
   end
+  
+  
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
@@ -23,5 +100,17 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
+ 
+ 
+
+   
 
 end
+   
+
+
+
+
+
+  
+  
